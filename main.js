db25367650941ea7ff5be0d3054c3742bfea29a4
dacer521 <davidluna0820@gmail.com>
2025-05-22 12:08:28 -0400

    removed devtools

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createWindow() {
         },
     });
     
-    mainWindow.webContents.openDevTools({ mode: "detach" });
+
     mainWindow.loadFile("index.html");
 
     const menu = Menu.buildFromTemplate([
